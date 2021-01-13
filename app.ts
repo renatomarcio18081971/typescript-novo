6f5f85c46638f7992820bfc39aa1efd8948db828
renatomarcio18081971 <renato.marcio@gmail.com>
2021-01-13 19:17:34 -0300

    teste

---
+++ app.ts
@@ -0,0 +1,5 @@
+let ola = (nome: string, idade: number) => {
+    console.log('ola ' + nome + ' - ' + idade)
+}
+
+ola('renato', 49)
---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+var ola = function (nome, idade) {
+    console.log('ola ' + nome + ' - ' + idade);
+};
+ola('renato', 49);
